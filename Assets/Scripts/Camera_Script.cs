00ee2f4fae1d50f714f6f5c1819573c98ed1f488
qfeys <qfeys@skynet.be>
2018-12-01 12:47:16 +0100

    Camera improvements

--- Assets/Scripts/Camera_Script.cs
+++ Assets/Scripts/Camera_Script.cs
@@ -14,7 +14,8 @@ public class Camera_Script : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Vector3 mouse = camera.ScreenToWorldPoint(Input.mousePosition).normalized * 2;
+        Vector2 mouse2 = camera.ScreenToWorldPoint(Input.mousePosition) - player.position;
+        Vector3 mouse = (mouse2.sqrMagnitude > 9 ? mouse2.normalized * 3 : mouse2) / 3;
         transform.position = player.position + mouse + new Vector3(0, 0, -10);
 	}
 }
